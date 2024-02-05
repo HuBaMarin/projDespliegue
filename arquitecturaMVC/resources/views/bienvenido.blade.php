94d44c6a75fa3d2ed9abe5e8eead8d372be677c8
HuBaMarin <marinlosenlaces@gmail.com>
2024-02-05 21:18:23 +0100

    nuevo

---
+++ arquitecturaMVC/resources/views/bienvenido.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="es-ES">
+<head>
+    <!-- ... other head tags ... -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>Bienvenido</title>
+</head>
+<body>
+<!-- ... body content ... -->
+</body>
+</html>
