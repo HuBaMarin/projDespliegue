3cc5f34a64000793a5de99246333ea5290d6fbc0
HuBaMarin <147373478+HuBaMarin@users.noreply.github.com>
2024-04-02 13:09:59 +0200

    Delete codigo/proyectoDespliegue directory

--- codigo/proyectoDespliegue/resources/views/components/layout/navbar.blade.php
+++
@@ -1,9 +0,0 @@
-<nav class="h-10v bg-nav flex flex-row justify-center items-center
-space-x-3 p-1">
-
-    <a href="inicio" class="btn btn-sm  btn-neutral">Inicio</a>
-    <a href="entrenamientos" class="btn  btn-sm  btn-primary">Entrenamientos</a>
-    <a href="ofertas" class="btn  btn-sm  btn-info">Ofertas</a>
-    <a href="actividades" class="btn  btn-sm">Actividades</a>
-
-</nav>
